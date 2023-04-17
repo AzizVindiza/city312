d6bc713e68a954487ceb09ba21f24b7964eea9aa
Vindiza Aziz <vindizaaziz@gmail.com>
2023-04-17 11:17:21 +0600

    responsive seconde section

--- src/pages/Home/FirstSection/FirstSection.jsx
+++ src/pages/Home/FirstSection/FirstSection.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import "./FirstSection.sass"
 import img from './FirstSection__img.png'
 import img640 from './FirstSection__img640.png'
+import img768 from './FirstSection__img768.png'
+import img1025 from './FirstSection__img1025.png'
+import img1440 from './FirstSection__img1440.png'
 import bg1 from './FirstSection__bg1.png'
 import bg1640 from './FirstSection__bg1640.png'
-import bg2640 from './FirstSection__bg2640.png'
+import bg1768 from './FirstSection__bg1768.png'
+import bg11024 from './FirstSection__bg11024.png'
+import bg11440 from './FirstSection__bg11440.png'
 import bg2 from './FirstSection__bg2.png'
+import bg2640 from './FirstSection__bg2640.png'
+import bg2768 from './FirstSection__bg2768.png'
+import bg21024 from './FirstSection__bg21024.png'
+import bg21440 from './FirstSection__bg21440.png'
+
 
 const FirstSection = () => {
     return (
@@ -25,6 +35,9 @@ const FirstSection = () => {
                     </div>
                     <div className="firstSection__img">
                         <picture>
+                            <source media="(min-width:1439px)" srcSet={img1440}/>
+                            <source media="(min-width:1025px)" srcSet={img1025}/>
+                            <source media="(min-width:768px)" srcSet={img768}/>
                             <source media="(min-width:640px)" srcSet={img640}/>
                             <img src={img} alt="312 card"/>
                         </picture>
@@ -37,17 +50,20 @@ const FirstSection = () => {
                     </div>
                     <div className="firstSection__card">
                         <h3 className="firstSection__card-numb">13000</h3>
-                        <p className="firstSection__card-p">довольных
+                        <p className="firstSection__card-p">довольных <br/>
                             пользвателей</p>
                     </div>
                     <div className="firstSection__card">
                         <h3 className="firstSection__card-numb">100+</h3>
-                        <p className="firstSection__card-p">предложений
+                        <p className="firstSection__card-p">предложений <br/>
                             скидок</p>
                     </div>
                 </div>
                 <div className="firstSection__bg1">
                     <picture>
+                        <source media="(min-width:1440px)" srcSet={bg11440}/>
+                        <source media="(min-width:1024px)" srcSet={bg11024}/>
+                        <source media="(min-width:768px)" srcSet={bg1768}/>
                         <source media="(min-width:640px)" srcSet={bg1640}/>
                         <img src={bg1} alt="bg"/>
                     </picture>
@@ -55,6 +71,9 @@ const FirstSection = () => {
                 </div>
                 <div className="firstSection__bg2">
                     <picture>
+                        <source media="(min-width:1440px)" srcSet={bg21440}/>
+                        <source media="(min-width:1024px)" srcSet={bg21024}/>
+                        <source media="(min-width:768px)" srcSet={bg2768}/>
                         <source media="(min-width:640px)" srcSet={bg2640}/>
                         <img src={bg2} alt="bg"/>
                     </picture>
--- src/pages/Home/MapSection/MapSection.jsx
+++ src/pages/Home/MapSection/MapSection.jsx
@@ -11,6 +11,7 @@ const MapSection = () => {
             </h2>
             <div className="map__google">
                 <iframe
+                    title="mapX"
                     src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d826.4433335598899!2d74.59557587177157!3d42.87450592458347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1679463495675!5m2!1sru!2skg"
                     style={{border: "0"}} allowFullScreen="" loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"></iframe>
