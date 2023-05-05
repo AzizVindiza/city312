9679c3616078523140c3a4161251aed99feda11c
Aybekmamyshev <aibekm591@gmail.com>
2023-05-05 12:29:55 +0600

    add redux  persisit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {PersistGate} from "redux-persist/integration/react";
+import {persist} from "./redux/store";
+import {Provider} from "react-redux";
+import store from "./redux/store"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persist}>
+            <App/>
+        </PersistGate>
+    </Provider>
 );
