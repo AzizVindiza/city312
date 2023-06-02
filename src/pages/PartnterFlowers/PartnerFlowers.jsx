ce191c44b0bc06f14fd9cf5d8932b5bfe3bab5fd
shikaridoki <rasul.vindiza@gmail.com>
2023-06-02 17:58:52 +0600

    partnerCArd

---
+++ src/pages/PartnterFlowers/PartnerFlowers.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PartnerCard from "./PartnerCard/PartnerCard";
+import PartnerSearch from "./PartnerSearch/PartnerSearch";
+
+const PartnerFlowers = () => {
+    return (
+        <>
+            <PartnerSearch/>
+
+        </>
+    );
+};
+
+export default PartnerFlowers;
