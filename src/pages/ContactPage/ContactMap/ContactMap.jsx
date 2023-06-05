57e4890a70ba5a105553f243a98c44f021beedaf
Aziz Vindiza <vindizaaziz@gmail.com>
2023-06-05 15:45:31 +0600

    fix mappage

--- src/pages/ContactPage/ContactMap/ContactMap.jsx
+++ src/pages/ContactPage/ContactMap/ContactMap.jsx
@@ -9,29 +9,16 @@ const ContactMap = () => {
             <div className="contactMap__container container">
                 <div className="contactMap__iframe">
                     <iframe
-                        title="map"
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4607.7357179856745!2d74.60042399242414!3d42.87863699970236!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7e74697b7a5%3A0xb625d929c04e872e!2z0J_QsNGA0Log0J_QsNC90YTQuNC70L7QstCw!5e0!3m2!1sru!2skg!4v1682581919768!5m2!1sru!2skg"
-                        allowFullScreen="" loading="lazy"
+                        title="mapX"
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d826.4433335598899!2d74.59557587177157!3d42.87450592458347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1679463495675!5m2!1sru!2skg"
+                        style={{border: "0"}} allowFullScreen="" loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div className="contactMap__wrapper">
                     <div className="contactMap__text">
                         <h2 className="contactMap__h2">Наш адрес </h2>
-                        <span className="contactMap__span">Бишкек</span>
+                        <span className="contactMap__span">Кыргызская республика, г. Бишкек, ул. Тоголок Молдо 3Б</span>
                     </div>
-                    <div className="contactMap__text">
-                        <h2 className="contactMap__h2">Наш адрес </h2>
-                        <span className="contactMap__span">Бишкек</span>
-                    </div>
-                    <div className="contactMap__text">
-                        <h2 className="contactMap__h2">Наш адрес </h2>
-                        <span className="contactMap__span">Бишкек</span>
-                    </div>
-                    <div className="contactMap__text">
-                        <h2 className="contactMap__h2">Наш адрес </h2>
-                        <span className="contactMap__span">Бишкек</span>
-                    </div>
-
                 </div>
 
             </div>
