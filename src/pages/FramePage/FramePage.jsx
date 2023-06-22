738dee4dfd8c10d82e50288e0583cbc1124075be
Aybekmamyshev <aibekm591@gmail.com>
2023-06-22 12:18:39 +0600

    framerpage,swiperfirst

--- src/pages/FramePage/FramePage.jsx
+++ src/pages/FramePage/FramePage.jsx
@@ -4,14 +4,17 @@ import WrapperSection from "./WrapperSection/WrapperSection";
 import TextSection from "./TextSection/TextSection";
 import MapSection from "./MapSection/MapSection";
 import CometSection from "./CommetSection/CometSection";
+import SwiperFirst from "./SwiperFirst/SwiperFirst";
 
 const FramePage = () => {
     return (
         <>
            <WrapperSection/>
             <TextSection/>
+            <SwiperFirst title={'Смотрете другие товары'}/>
             <MapSection/>
             <CometSection/>
+            <SwiperFirst title={'Действующие акции'}/>
 
         </>
     );
